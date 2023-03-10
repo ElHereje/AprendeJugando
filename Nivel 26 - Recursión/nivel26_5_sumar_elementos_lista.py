25c7b809448475370c0f7532b91729f37078b065
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-10 19:18:48 +0100

    Add files via upload

---
+++ Nivel 26 - Recursión/nivel26_5_sumar_elementos_lista.py
@@ -0,0 +1,41 @@
+'''
+Sumar los elementos de una lista con una función de manera recursiva
+
+lista = [1, 2, 3, 4, 5]
+
+hacerlo de una manera iterativa es sencillo:
+
+def sumar(lista):
+    suma = 0
+    for i in lista:
+        suma += i
+    return suma
+
+
+Para hacerlo de manera recursiva, lo 1º es buscar el caso base.
+Lo mas simple es el caso de una lista vacía
+
+lista = [] --> suma = 0
+lista = [10] --> suma = 10 + suma[0]
+lista = [10, 20] --> suma = 10 + suma[20] --> 10 + 20 + suma[]
+lista = [10, 20, 30] --> suma = 10 + suma[20 + 30] --> 10 + 20 + suma[30]
+...
+
+en nuestro caso
+suma = 1 + [2,3,4,5]
+            2 + [3+4+5]
+                3 + [4+5]
+                    4 + [5]
+                        5 + []
+
+
+'''
+lista = [1, 2, 3, 4, 5]
+
+def sumar(lista):
+    if lista == []:
+        suma = 0
+    else:
+        suma = lista[0] + sumar(lista[1:])
+    return suma
+print(sumar(lista))
---
+++ Nivel 26 - Recursión/nivel26_6_invertir_lista.py
@@ -0,0 +1,6 @@
+'''
+Invertir una lista de manera recursiva:
+
+lista = [1,2,3,4,5] --> inversa = [5,4,3,2,1]
+
+'''
