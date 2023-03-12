930abb9b4a606947ed62a74b7a44c6b5c74eb2d4
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-12 19:41:33 +0100

    Update nivel26_6_invertir_lista.py

--- Nivel 26 - Recursión/nivel26_6_invertir_lista.py
+++ Nivel 26 - Recursión/nivel26_6_invertir_lista.py
@@ -4,3 +4,13 @@ Invertir una lista de manera recursiva:
 lista = [1,2,3,4,5] --> inversa = [5,4,3,2,1]
 
 '''
+
+lista = [1,2,3,4,5]
+
+def inversa(lista):
+    if lista == []:
+        return lista
+    else:
+        return inversa(lista[1:]) + lista[:1]
+    
+print(inversa(lista))
