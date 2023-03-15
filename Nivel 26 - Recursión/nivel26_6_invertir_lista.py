491c17d6876fcf51b2a6cc271e56d06572fa5266
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-15 17:38:37 +0100

    Update nivel26_6_invertir_lista.py

--- Nivel 26 - Recursión/nivel26_6_invertir_lista.py
+++ Nivel 26 - Recursión/nivel26_6_invertir_lista.py
@@ -5,7 +5,13 @@ lista = [1,2,3,4,5] --> inversa = [5,4,3,2,1]
 
 En resumen, lo que se hace es eliminar el último elemento y posicionarlo en primer lugar
 
-'''
+lista = [1,2] --> [2] + [1]
+lista = [1] --> [1] + []
+lista = [] --> []  -> caso base
+
+lista = [1,2,3] --> [3]+[1,2] --> [3]+[2]+[1]
+
+
 
 lista = [1,2,3,4,5]
 
@@ -28,3 +34,23 @@ def inversa_2(lst):
         return [lst[-1]] + inversa_2(lst[:-1])
 
 print(inversa_2(lista))
+
+'''
+
+lista = [1,2,3,4,5]
+
+# def invertir_lista(lista):
+#     if lista == []:
+#         inversa = lista
+#     else:
+#         inversa = [lista[-1]] + invertir_lista(lista[:-1]) # último + el resto (al que hacemos recursiva)
+#     return inversa
+
+# esto podemos optimizarlo:
+def invertir_lista(lista):
+    if lista == []:
+        return lista
+    else:
+        return [lista[-1]] + invertir_lista(lista[:-1])
+    
+print(invertir_lista(lista))
