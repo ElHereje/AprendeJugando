2cd17502c95184adebd4532701aa2d64bbea1a82
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-25 20:02:35 +0100

    Update nivel26_12_crear_listas_pares.py

--- Nivel 26 - Recursión/nivel26_12_crear_listas_pares.py
+++ Nivel 26 - Recursión/nivel26_12_crear_listas_pares.py
@@ -2,3 +2,16 @@
 Crear una lista de forma recursiva con los números pares de 
 entre 2 números dados
 '''
+
+
+def lista_pares(inicio, final):
+    # caso base --> inicio >= final
+    if inicio > final:
+        return []
+    elif inicio % 2 == 0:
+        return [inicio] + lista_pares(inicio+1, final)
+    else:
+        return lista_pares(inicio+1, final)
+    
+    
+print(lista_pares(2, 10))
