ae78c66d76c418de478b5db55774dd075030d94b
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-25 20:02:53 +0100

    Add files via upload

---
+++ Nivel 26 - Recursión/nivel26_13_sumar_elementos_pares.py
@@ -0,0 +1,8 @@
+'''
+Función que halle de fora recursiva el resultado
+de sumar los nºs pares de una lista
+- Estrategia 1: por sublistas
+- Estrategia 2: por índices
+'''
+
+lista = [3, 4, 5, 8, 9, 12]
