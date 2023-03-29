b07decbd29412fe50e797b3024328ca5f9153cf5
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-29 14:41:57 +0200

    Add files via upload

---
+++ Nivel 26 - Recursión/Nivel 27 - POO/N1_juego_zombie/persona.py
@@ -0,0 +1,6 @@
+class Persona:
+    
+    # inicializamos los objetos de la clase
+    # self hace referencia a cada uno de los objetos
+    def __init__(self, nombre):
+        self.nombre = nombre
