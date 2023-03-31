0873381cc191e24d3b5616014d90210d1f401fa1
Almadieis <59013338+ElHereje@users.noreply.github.com>
2023-03-31 18:02:38 +0200

    Delete Nivel 26 - Recursión/Nivel 27 - POO directory

--- Nivel 26 - Recursión/Nivel 27 - POO/N1_juego_zombie/persona.py
+++
@@ -1,6 +0,0 @@
-class Persona:
-    
-    # inicializamos los objetos de la clase
-    # self hace referencia a cada uno de los objetos
-    def __init__(self, nombre):
-        self.nombre = nombre
